9bf8aaf955549364f86af13763dfa8be2dbb7eae
B2MW <P5@Champion.local>
2014-09-30 15:00:32 -0500

    Adjective and Name View Controllers and Classes created

---
+++ WordPlay/EnterAdjectiveViewController.h
@@ -0,0 +1,14 @@
+//
+//  EnterAdjectiveViewController.h
+//  WordPlay
+//
+//  Created by Bradley Walker on 9/30/14.
+//  Copyright (c) 2014 BlackSummerVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterAdjectiveViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *adjectiveTextField;
+
+@end
---
+++ WordPlay/EnterNameViewController.h
@@ -0,0 +1,14 @@
+//
+//  EnterNameViewController.h
+//  WordPlay
+//
+//  Created by Bradley Walker on 9/30/14.
+//  Copyright (c) 2014 BlackSummerVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterNameViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+
+@end
