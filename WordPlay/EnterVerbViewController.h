921866cbee69e362937a9903b391a95dbddbba6e
B2MW <P5@Champion.local>
2014-09-30 17:46:16 -0500

    Added 2 View Controllers for add'l parts of speech

---
+++ WordPlay/EnterVerbViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnterVerbViewController.h
+//  WordPlay
+//
+//  Created by Bradley Walker on 9/30/14.
+//  Copyright (c) 2014 BlackSummerVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterVerbViewController : UIViewController
+
+@end
